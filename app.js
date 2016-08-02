258f46b009c22dfc27400b874140fe0a5ebbceb7
heatsink <seanswally@gmail.com>
2016-08-01 18:50:21 -1000

    Add button organization & content

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = angular.module('resumeApp', ['ngMaterial'])
     '200': 'ef9a9a',
     '300': 'e57373', //Text ONE TWO THREE
     '400': 'efefef',
-    '500': '0090ff', // BUTTON COLOR
+    '500': '0090ff', // TAB & TOOLBAR COLOR
     '600': '0090ff', 
     '700': 'd32f2f',
     '800': 'c62828',
@@ -34,73 +34,3 @@ app.controller('personCtrl', function($scope) {
         $scope.myVar = !$scope.myVar;
     };
 });
-
-
-
-/**
-Copyright 2016 Google Inc. All Rights Reserved. 
-Use of this source code is governed by an MIT-style license that can be in foundin the LICENSE file at http://material.angularjs.org/license.
-**/
-
-
-
-/*
-  app.controller('sideNavController', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.toggleLeft = buildDelayedToggler('left');
-    $scope.toggleRight = buildToggler('right');
-    $scope.isOpenRight = function(){
-      return $mdSidenav('right').isOpen();
-    };
-    /**
-    * Supplies a function that will continue to operate until the
-    * time is up.
-    */
- /*
-    function debounce(func, wait, context) {
-      var timer;
-      return function debounced() {
-        var context = $scope,
-        args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
-    /**
-    * Build handler to open/close a SideNav; when animation finishes
-    * report completion in console
-    */
- /*
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-        .toggle()
-        .then(function () {
-          $log.debug("toggle " + navID + " is done");
-        });
-      }, 200);
-    }
-    function buildToggler(navID) {
-      return function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-        .toggle()
-        .then(function () {
-          $log.debug("toggle " + navID + " is done");
-        });
-      }
-    }
-  });
-  app.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      // Component lookup should always be available since we are not using `ng-if`
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
-    };
-  });
-*/
